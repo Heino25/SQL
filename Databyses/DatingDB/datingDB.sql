32322490195fff0a0a93d6d9517b593f07b4635f
Heino <heinoduplessis@gmail.com>
2021-07-20 12:51:39 +0200

    insert values

--- Databyses/DatingDB/datingDB.sql
+++ Databyses/DatingDB/datingDB.sql
@@ -20,13 +20,13 @@ CREATE TABLE profession (
 SELECT * FROM profession
 
 CREATE TABLE zip_code (
- zip_code bigserial,
  city varchar(25),
  province varchar(25),
- CONSTRAINT check_zip CHECK (zip_code <= 4),
- CONSTRAINT city_unique UNIQUE (city),
+ zip_code bigserial CHECK (LENGTH (CAST(zip_code AS varchar(4))) <= 4),
  CONSTRAINT zip_key PRIMARY KEY (zip_code)
 );
+
+DROP TABLE zip_code CASCADE
 
 SELECT * FROM zip_code
 
@@ -57,7 +57,7 @@ SELECT * FROM my_contacts
 
 CREATE TABLE interests (
  interests_id bigserial,
- intersets varchar(50),
+ interests varchar(50),
  CONSTRAINT intr_key PRIMARY KEY (interests_id)
 );
 
@@ -67,6 +67,8 @@ CREATE TABLE contact_interests (
  contact_id bigserial REFERENCES my_contacts (contact_id),
  interests_id bigserial REFERENCES interests (interests_id) 
 );
+
+DROP TABLE interests CASCADE
 
 SELECT * FROM contact_interests
 
@@ -109,8 +111,104 @@ VALUES (9300, 'Bloemfontein', 'Freestate'),
 (4001, 'KwaZulu-Natal', 'Amamzintoti'),
 (5001, 'Eastern Cape', 'George'),
 (6001, 'Western Cape', 'Stellenbosch'),
-(2553, 'Gauteng', 'Vanderbijl'),
+(2554, 'Gauteng', 'Vanderbijl'),
 (3443, 'Northern Cape', 'Kanon Eiland'),
 (4559, 'Limpopo', 'Tzaneen'),
 (2553, 'North West', 'Potchefstroom'),
-(0094, 'Mpumalanga', 'Volksrust'),
+(0094, 'Mpumalanga', 'Volksrust');
+
+INSERT INTO Status (status)
+VALUES ('Single'),
+('Married'),
+('Widowed'),
+('In a Relationship'),
+('Divorced'),
+('Open Relationship');
+
+INSERT INTO interests (interests)
+VALUES ('Offroad Tours'),
+('Gaming'),
+('Singing'),
+('Reading'),
+('Dancing'),
+('Netflix'),
+('Hiking'),
+('Drinking'),
+('Travel'),
+('Shopping'),
+('Art'),
+('Ski Diving'),
+('Cooking');
+
+INSERT INTO seeking (seeking)
+VALUES ('Relationship'),
+('Friendship');
+
+INSERT INTO my_contacts (last_name, first_name, phone, email, gender, birthday, prof_id, zip_code, status_id)
+VALUES ('Sauer', 'JennySauer', '0872341234', 'jenny@gmail.com', 'F', '2000-01-05', 1, 9300, 1),
+('Smith', 'Erin', '0835716666', 'erin@gmail.com', 'F', '1998-09-17', 2, 4000, 5),
+('Cooper', 'Talika', '0866325786', 'cooper@gmail.com', 'F', '2000-01-30', 3, 5000, 2),
+('Pollard', 'Handre', '0859100084', 'pollard@gmail.com', 'M', '1995-03-13', 4, 6000, 1),
+('Dupteez', 'Cortney', '0842999670', 'cortney@gmail.com', 'F', '1998-04-16', 5, 2551, 6),
+('Bosch', 'Josh', '0835636030', 'josh@gmail.com', 'M', '1990-12-16', 6, 3442, 1),
+('Gaga', 'Tyler', '0839714819', 'Gaga@gmail.com', 'M', '1987-05-05', 7, 4559, 4),
+('Foster', 'Britney', '0834307963', 'britney@gmail.com', 'F', '1993-07-18', 8, 3441, 4),
+('Fox', 'Megan', '0825138355', 'fox@gmail.com', 'F', '1992-09-10', 1, 0095, 3),
+('Synidious', 'Jolleen', '0821185849', 'Jolleen@gmail.com', 'M', '2000-04-29', 2, 9301, 2),
+('Chang', 'Dhiao', '0838839728', 'chang@gmail.com', 'F', '1997-01-08', 2, 4001, 1),
+('Lincoln', 'Abe', '0828266837', 'abe@gmail.com', 'M', '1994-02-08', 3, 5001, 6),
+('Ericson', 'Jay', '0846087530', 'jay@gmail.com', 'M', '1997-04-04', 4, 4559, 1),
+('Kingley', 'Justin', '0825558574', 'justin@gmail.com', 'M', '1991-11-22', 5, 2553, 6),
+('Naudee', 'Charne', '0832358342', 'naudee@gmail.com', 'F', '2001-07-03', 6, 0094, 1);
+
+SELECT * FROM my_contacts
+
+INSERT INTO contact_interests (contact_id, interests_id)
+VALUES (1, 1),
+(2, 2),
+(3, 3),
+(4, 3),
+(5, 4),
+(6, 5),
+(7, 6),
+(8, 7),
+(9, 8),
+(10, 9),
+(11, 10),
+(12, 11),
+(13, 12),
+(14, 13),
+(15, 11),
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 8),
+(8, 7),
+(9, 11),
+(10, 5),
+(11, 10),
+(12, 1),
+(13, 2),
+(14, 8),
+(15, 12),
+(1, 3),
+(2, 13),
+(3, 6),
+(4, 7),
+(5, 12),
+(6, 10),
+(7, 8),
+(8, 2),
+(9, 4),
+(10, 9),
+(11, 6),
+(12, 3),
+(13, 5),
+(14, 1),
+(15, 11);
+
+INSERT INTO contact_seeking (contact_id, seeking_id)
+VALUES (, ),
