de5ea56288c80e93470813a4fe1383ef37011b05
Heino <heinoduplessis@gmail.com>
2021-07-27 15:59:52 +0200

    advanced sets

---
+++ Databyses/Python_PostgreSQL/Python Referesher/advanced.set.opperation.py
@@ -0,0 +1,11 @@
+# friends = {"Rolf", "Anne"}
+# abroad = {"Bob", "Anne"}
+
+# local_friends = abroad.difference(friends)
+# print(local_friends)
+
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science = {"Bob,", "Jen", "Adam", "Anne"}
+
+both = art.intersection(science)
+print(both)
