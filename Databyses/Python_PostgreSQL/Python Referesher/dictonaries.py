8ef997be7dfcdb27104ecec6261b506539963841
Heino <heinoduplessis@gmail.com>
2021-07-28 13:13:30 +0200

    dictonaries

---
+++ Databyses/Python_PostgreSQL/Python Referesher/dictonaries.py
@@ -0,0 +1,19 @@
+# friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+
+# friend_ages["Rolf"] = 20
+
+# print(friend_ages)
+
+# friends = [
+#     {"name": "Rolf", "age": 24},
+#     {"name": "Adam", "age": 30},
+#     {"name": "Anne", "age": 27},
+# ]
+
+# print(friends)
+# print(friends[1]["name"])
+
+students_attendence = {"Rolf": 96, "Bob": 80, "Anne": 100}
+
+for student, attendence in students_attendence.items():
+    print(f"{student}: {attendence}")
