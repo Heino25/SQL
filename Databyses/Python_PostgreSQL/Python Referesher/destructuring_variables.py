6c50b4969ce06f2c3f247d0edc635b2135942321
Heino <heinoduplessis@gmail.com>
2021-07-28 13:47:13 +0200

    destructuring variables

---
+++ Databyses/Python_PostgreSQL/Python Referesher/destructuring_variables.py
@@ -0,0 +1,9 @@
+# t = 5, 11
+# x, y = t
+
+# print(x, y)
+
+people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+
+for person in people:
+    print(f"Name: {person[0]}, Age: {person[1]}, Profession: {person[2]}")
