e2f5fc4d25da906c70645a8c766795b3a1006530
Heino <heinoduplessis@gmail.com>
2021-08-02 10:54:05 +0200

    decorations with parameters

--- Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
+++ Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
@@ -1,25 +1,36 @@
 import functools
 
-user = {"username": "Jose", "access_level": "admin"} # guest/admin
+user = {"username": "Jose", "access_level": "guest"} # guest/admin
 
 
-def make_secure(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if user["access_level"] == "admin":
-            return func(*args, **kwargs)
-        else:
-            return f"No admin permission for {user['username']}."
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permission for {user['username']}."
 
 
-    return secure_function
-
-@make_secure
-def get_password(panel):
-    if panel == "admin":
-        return "1234"
-    elif panel == "billing":
-        return "super_secure_password"
+        return secure_function
+    return decorator
 
 
-print(get_password("billing"))
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 1234"
+
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+print(get_admin_password())
+print(get_dashboard_password())
+
+user = {"username": "anna", "access_level": "admin"}
+
+print(get_admin_password())
+print(get_dashboard_password())
