ec6e13b11b7c315190244c51007823607f968b60
Heino <heinoduplessis@gmail.com>
2021-08-02 10:29:04 +0200

    decorations with parameters

--- Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
+++ Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
@@ -1,20 +1,25 @@
+import functools
+
 user = {"username": "Jose", "access_level": "admin"} # guest/admin
 
+
 def make_secure(func):
-    def secure_function():
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
         else:
             return f"No admin permission for {user['username']}."
 
 
     return secure_function
 
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
 
-def get_admin_password():
-    return "1234"
 
-
-get_admin_password = make_secure(get_admin_password)
-
-print(get_admin_password())
+print(get_password("billing"))
