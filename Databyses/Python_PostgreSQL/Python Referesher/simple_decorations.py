704032512ce5c45a7445c3c9f69ae1d8e180860e
Heino <heinoduplessis@gmail.com>
2021-08-02 10:08:50 +0200

    decorations

--- Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
+++ Databyses/Python_PostgreSQL/Python Referesher/simple_decorations.py
@@ -1,7 +1,20 @@
-user = {"username": "jose", "access_level": "guest"}
+user = {"username": "Jose", "access_level": "admin"} # guest/admin
+
+def make_secure(func):
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"No admin permission for {user['username']}."
+
+
+    return secure_function
 
 
 def get_admin_password():
     return "1234"
+
+
+get_admin_password = make_secure(get_admin_password)
 
 print(get_admin_password())
