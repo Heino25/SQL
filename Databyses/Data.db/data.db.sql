98836d309ed46ac331d0d3bc22ad7ca065530e9e
Heino <heinoduplessis@gmail.com>
2021-08-03 10:55:44 +0200

    data.db

---
+++ Databyses/Data.db/data.db.sql
@@ -0,0 +1,14 @@
+-- Database: data.db
+
+-- DROP DATABASE "data.db";
+
+CREATE DATABASE "data.db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE TABLE "users"
