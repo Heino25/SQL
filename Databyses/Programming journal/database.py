0254b351846f9b96f22bd46607bc87c36d9f738a
Heino <heinoduplessis@gmail.com>
2021-08-04 07:34:41 +0200

    retrieving results

--- Databyses/Programming journal/database.py
+++ Databyses/Programming journal/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-connection = sqlite3.connect("data.db.sql")
-
+connection = sqlite3.connect("data.db")
+connection.row_factory = sqlite3.Row
 
 def create_table():
     with connection:
@@ -17,4 +17,5 @@ def add_entry(entry_content, entry_date):
 
 
 def get_entries():
-    return entries
+    cursor = connection.execute("SELECT * FROM entries;")
+    return cursor
