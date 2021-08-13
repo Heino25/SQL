93eaf75a37519be8625785a970a9156c75fe94c2
Heino <heinoduplessis@gmail.com>
2021-08-13 11:26:34 +0200

    corrections2

--- Databyses/Poll/database.py
+++ Databyses/Poll/database.py
@@ -4,12 +4,15 @@ from contextlib import contextmanager
 
 Poll = Tuple[int, str, str]
 Option = Tuple[int, str, int]
-Vote = Tuple[str, int]
+Vote = Tuple[str, int, str]
 
 
-CREATE_POLLS = "CREATE TABLE IF NOT EXISTS polls (id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);"
-CREATE_OPTIONS = "CREATE TABLE IF NOT EXISTS options (id SERIAL PRIMARY KEY, option_text TEXT, poll_id INTEGER);"
-CREATE_VOTES = "CREATE TABLE IF NOT EXISTS votes (username TEXT, option_id INTEGER, vote_timestamp INTEGER);"
+CREATE_POLLS = """CREATE TABLE IF NOT EXISTS polls
+(id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT)"""
+CREATE_OPTIONS = """CREATE TABLE IF NOT EXISTS options
+(id SERIAL PRIMARY KEY, option_text TEXT, poll_ID INTEGER, FOREIGN KEY(poll_id) REFERENCES polls (id));"""
+CREATE_VOTES = """CREATE TABLE IF NOT EXISTS votes
+(username TEXT, option_id INTEGER, vote_timestamp INTEGER, FOREIGN KEY (option_id) REFERENCES options (id));"""
 
 
 SELECT_ALL_POLLS = "SELECT * FROM polls;"
