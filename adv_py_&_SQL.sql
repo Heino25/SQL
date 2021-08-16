66b051fdb32b58dd308210d3302d26a1e0cf41a2
Heino <heinoduplessis@gmail.com>
2021-08-16 08:47:09 +0200

    user defined

---
+++ adv_py_&_SQL.sql
@@ -0,0 +1,32 @@
+-- Database: Python and advanced postgreSQL with psycopg2
+
+-- DROP DATABASE "Python and advanced postgreSQL with psycopg2";
+
+CREATE DATABASE "Python and advanced postgreSQL with psycopg2"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE TABLE users (name TEXT, email TEXT, last_opend INTEGER);
+
+INSERT INTO users VALUES ('Bob Smith', 'bobsmith@example.com', 65615),
+('Rolf Smith', 'rolf@gmail.com', 8586854),
+('Susan williams', 'Susan@gmail.com', 56),
+('Anne pun', 'ana@gmail.com', 2456789);
+
+DELETE FROM users
+
+CREATE FUNCTION delete_inactive(seconds NUMERIC) RETURNS void AS $$
+	DELETE FROM users WHERE last_opend > seconds;
+$$ LANGUAGE SQL;
+
+SELECT * FROM users
+
+SELECT delete_inactive(50000);
+
+DROP FUNCTION delete_inactive(NUMERIC);
+
