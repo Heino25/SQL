cd8666dcd0737823f06b67463a0316a7abc66662
Heino <heinoduplessis@gmail.com>
2021-08-18 08:54:18 +0200

    graph test

--- Databyses/Matplotlib/Graph_test.py
+++ Databyses/Matplotlib/Graph_test.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 
 plt.figure()
 
-plt.xlabel("Categories")
-plt.ylabel("Amounts")
-plt.title("Categories vs Amounts")
+# plt.plot([1, 2, 3, 4], [3, 5, 9, 25])
+# plt.show()
 
-lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9])
-plt.show()
+
+####
+
+
+# plt.xlabel("Categories")
+# plt.ylabel("Amounts")
+# plt.title("Categories vs Amounts")
+#
+# lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9])
+# plt.show()
